627dc7b00b59d710f32fae3f48bca63059ea8333
Elias Molina <elias.m.v.2004@gmail.com>
2025-03-27 22:51:07 +0100

    ejercicio 5

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
 window.addEventListener('load', function(event) {
+    // 1. Una variable numero cuyo valor sea -4.
     let numero = -4;
+    // 2. Una variable palabra cuyo valor sea ”Hola Mundo”
     let palabra = "Hola Mundo";
+    // 3. Una variable booleana cuyo nombre sea respuesta e inicialmente con valor true
     let respuesta = true;
+    // 4. Crea una constante con nombre pi cuyo valor sea 3.14
     const PI = 3.14;
+    // 5. Modifica el valor de la variable numero para que en lugar de -4 sea 54 pero sin tocar la parte de la declaración de la variable.
+    numero = 54;
+    // 6. ¿Qué pasa si modificas el valor de la variable antes de que esta sea declarada?
+    // 7. Muestra por consola cada una de las variables. Usa console.log(nombre_variable); 
+    // 8. Declara 2 variables y asigna los valores 5 y 2 respectivamente
+    // 9. Muestra por consola la suma de ambas variables.
+    // 10. Muestra por consola la resta de ambas variables.
+    // 11. Muestra por consola la multiplicación de ambas variables.
+    // 12. Muestra por consola la división de ambas variables.
+    // 13. Muestra por consola la módulo de ambas variables.
+    // 14. Declara una variable cuyo valor sea tu nombre.
+    // 15. Muestra por consola el JAVASCRIPT texto Hola concatenado con el valor de la variable anterior.
+    
 });
